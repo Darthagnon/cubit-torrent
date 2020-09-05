1c48e9f9e4b44904cdebbda9f4612634745f22b3
Darren John de Lima <darren.delima@yahoo.co.uk>
2020-09-05 19:11:51 +0100

    Update jdownloader.py
    add widgets (source: https://github.com/biplobsd/RLabClone/blob/master/res/rlab_utils.py)

--- jdownloader.py
+++ jdownloader.py
@@ -1,3 +1,124 @@
+import json
+import re  # nosec
+import ipywidgets as widgets  # pylint: disable=import-error
+from IPython.display import HTML, clear_output, display  # pylint: disable=import-error
+from google.colab import files  # pylint: disable=import-error
+from glob import glob
+from sys import exit as exx
+
+# Ultilities Methods ==========================================================
+
+
+def createButton(name, *, func=None, style="", icon="check"):
+    import ipywidgets as widgets  # pylint: disable=import-error
+
+    button = widgets.Button(
+        description=name, button_style=style, icon=icon, disabled=not bool(func)
+    )
+    button.style.font_weight = "900"
+    button.on_click(func)
+    output = widgets.Output()
+    display(button, output)
+
+
+def generateRandomStr():
+    from uuid import uuid4
+
+    return str(uuid4()).split("-")[0]
+
+
+def checkAvailable(path_="", userPath=False):
+    from os import path as _p
+
+    if path_ == "":
+        return False
+    else:
+        return (
+            _p.exists(path_)
+            if not userPath
+            else _p.exists(f"/usr/local/sessionSettings/{path_}")
+        )
+
+
+def findProcess(process, command="", isPid=False):
+    from psutil import pids, Process  # pylint: disable=import-error
+
+    if isinstance(process, int):
+        if process in pids():
+            return True
+    else:
+        for pid in pids():
+            try:
+                p = Process(pid)
+                if process in p.name():
+                    for arg in p.cmdline():
+                        if command in str(arg):
+                            return True if not isPid else str(pid)
+                        else:
+                            pass
+                else:
+                    pass
+            except:  # nosec
+                continue
+
+
+def runSh(args, *, output=False, shell=False):
+    import subprocess
+    import shlex  # nosec
+
+    if not shell:
+        if output:
+            proc = subprocess.Popen(  # nosec
+                shlex.split(args), stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+            )
+            while True:
+                output = proc.stdout.readline()
+                if output == b"" and proc.poll() is not None:
+                    return
+                if output:
+                    print(output.decode("utf-8").strip())
+        return subprocess.run(shlex.split(args)).returncode  # nosec
+    else:
+        if output:
+            return (
+                subprocess.run(
+                    args,
+                    shell=True,  # nosec
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                )
+                .stdout.decode("utf-8")
+                .strip()
+            )
+        return subprocess.run(args, shell=True).returncode  # nosec
+
+
+def accessSettingFile(file="", setting={}):
+    from json import load, dump
+
+    if not isinstance(setting, dict):
+        print("Only accept Dictionary object.")
+        exx()
+    fullPath = f"/usr/local/sessionSettings/{file}"
+    try:
+        if not len(setting):
+            if not checkAvailable(fullPath):
+                print(f"File unavailable: {fullPath}.")
+                exx()
+            with open(fullPath) as jsonObj:
+                return load(jsonObj)
+        else:
+            with open(fullPath, "w+") as outfile:
+                dump(setting, outfile)
+    except:
+        print(f"Error accessing the file: {fullPath}.")
+
+
+def memGiB():
+    from os import sysconf as _sc  # pylint: disable=no-name-in-module
+
+    return _sc("SC_PAGE_SIZE") * _sc("SC_PHYS_PAGES") / (1024.0 ** 3)
+
 # JDownloader =================================================================
 
 Email = widgets.Text(placeholder="*Required", description="Email:")
